16ae0309fb23b18e5435d0daab5cd0d594fd1dbd
Bagieta <>
2023-10-08 17:40:05 +0200

    Init commit

---
+++ main/Inc/wifi.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//WiFi
+#include "esp_log.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+
+//FreeRTOS
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
