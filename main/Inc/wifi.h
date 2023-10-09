d8348bcd58d82b9f7e696ca24ec07d66c280b470
Bagieta <>
2023-10-09 19:26:09 +0200

    'Wifi'

--- main/Inc/wifi.h
+++ main/Inc/wifi.h
@@ -4,8 +4,19 @@
 #include "esp_log.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
 
 //FreeRTOS
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
+
+#include "esp_system.h"
+#include <stdio.h>
+#include <string.h>
+
+//Main wifi connect function
+void connect_wifi(void);
+
+static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
